928c132492d142c1798473f2d98e77a3bba184fa
MileneBedouhene <milenebedouhene@gmail.com>
2024-07-26 20:38:38 +0100

    random

--- AES.py
+++ AES.py
@@ -9,13 +9,13 @@ def aes_decrypt(encrypted_flag, key):
     iv = b64decode(b64['iv'])
     ct = b64decode(b64['ciphertext'])
     cipher = AES.new(key, AES.MODE_CBC, iv)
-    pt = unpad(cipher.decrypt(ct), AES.block_size)
+    pt = cipher.decrypt(ct)
     return pt.decode('utf-8')
 
 # Exemple d'utilisation
 
-key =33101504605429929988144644818294662847610553853175879710826179671396651844626  
-encrypted_flag = '{"iv": "JyamZXPHvw49toRWlA/xgg==", "ciphertext": "xfR7oskDwQij6R1YPdi9eDdiHbj537RTKOOQA7755fOSSzf5lLjspFgwcul3TpqN"}'
+key =112374476872014624591276898161978042815099170765827274764402705083321630869961  
+encrypted_flag = '{"iv": "xTq149TR2u3yDOsTrEu8TQ==", "ciphertext": "IcqsfTIMS8V1ESD/a4VZwFcf2N/lOLvDGVtyPjW9DO1PNv7+D6sevNqtQiqT0UTY"}'
 
 decrypted_flag = aes_decrypt(encrypted_flag, key)
 print(decrypted_flag)
